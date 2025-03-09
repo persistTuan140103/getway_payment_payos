b2f97961d78f4aafc7957b885ee401c02a35e22a
persistTuan140103 <khanghoa7896@gmail.com>
2025-03-09 23:11:18 +0700

    first_init

---
+++ display_payment_app/app_flashsale.py
@@ -0,0 +1,65 @@
+import os, sys
+import time
+import pandas as pd
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect
+from payos import PayOS, ItemData, PaymentData
+
+load_dotenv()
+
+BASE_URL_PROJECT = os.path.abspath(os.path.join(os.path.dirname(__file__), *['..']*1))
+
+BASE_ANALYSIS_DIR = os.path.join(BASE_URL_PROJECT, 'data')
+PAYOS_CLIENT_ID = os.getenv("PAYOS_CLIENT_ID")
+PAYOS_API_KEY = os.getenv("PAYOS_API_KEY")
+PAYOS_CHECKSUM_KEY = os.getenv("PAYOS_CHECKSUM_KEY")
+# WEB_DOMAIN = os.getenv("WEB_DOMAIN")
+
+payos = PayOS(PAYOS_CLIENT_ID, PAYOS_API_KEY, PAYOS_CHECKSUM_KEY)
+# WEB_DOMAIN='http://backfriday.beenet.vn'
+WEB_DOMAIN= os.getenv("WEB_DOMAIN")
+
+app = Flask(__name__)
+
+@app.route("/")
+def display_products():
+    try:
+        data = pd.read_csv(os.path.join(BASE_ANALYSIS_DIR, 'analysis.csv'))
+        products = data.to_dict(orient="records")
+    except Exception as e:
+        return f"Error loading products: {e}", 500
+
+    return render_template("index.html", products=products)
+
+@app.route("/buy", methods=["POST"])
+def buy_product():
+    product_name = request.form.get("product_name")
+    current_price = request.form.get("current_price")
+
+    return render_template("buy_form.html", product_name=product_name, current_price=current_price)
+
+@app.route("/payment", methods=["POST"])
+def create_payment_link():
+    try:
+        product_name = request.form.get("product_name")
+        customer_name = request.form.get("buyerName")
+        phone_number = request.form.get("buyerPhone")
+        current_price = int(float(request.form.get("current_price")))
+
+        item = ItemData(name=f"{product_name} - {customer_name} ({phone_number})", quantity=1, price=current_price)
+        payment_data = PaymentData(
+            orderCode=int(time.time()),
+            amount=current_price,
+            description=f"Payment for {product_name}"[:25],
+            items=[item],
+            cancelUrl=WEB_DOMAIN,
+            returnUrl=WEB_DOMAIN
+        )
+        payment_link_response = payos.createPaymentLink(payment_data)
+    except Exception as e:
+        return "Erro: " +  str(e)
+
+    return render_template("payment_qr.html", checkout_url=payment_link_response.checkoutUrl)
+
+if __name__ == "__main__":
+    app.run(debug=True)
